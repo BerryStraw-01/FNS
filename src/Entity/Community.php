54f1409c8c7d656839bdf2f036b61772ad118d8d
kigawa <kigawa.8390@gmail.com>
2023-03-27 11:54:56 +0900

    wip

---
+++ src/Entity/Community.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class Community {
+
+}
